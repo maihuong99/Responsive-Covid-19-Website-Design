5ed3a6cfc01d52fa218f1ee2fe5687b4024d16c4
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-07-31 23:29:55 +0700

    complete contact section and blogs section

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var menu = document.querySelector('#menu');
 var nav = document.querySelector('.nav');
+var links = document.querySelectorAll('.nav a');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
@@ -10,3 +11,14 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     nav.classList.remove('active')
 }
+
+links.forEach(function(link){
+    link.onclick = () =>{
+        var parent = link.parentElement;
+        for(let i=0; i<parent.children.length; i++){
+            parent.children[i].classList.remove('active')
+        }
+        link.classList.add('active');
+    }
+});
+
