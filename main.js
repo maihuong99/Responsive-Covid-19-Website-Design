2d7a3fc45252adb85efa540bf71fb175851684f4
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-07-31 13:13:05 +0700

    complete home section and home section

---
+++ main.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector('#menu');
+var nav = document.querySelector('.nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    nav.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    nav.classList.remove('active')
+}
