7692c3938928a7c700be918cf8d530cbc9d6e736
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-08-01 22:05:27 +0700

    scroll spy

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var menu = document.querySelector('#menu');
 var nav = document.querySelector('.nav');
-var links = document.querySelectorAll('.nav a');
+var links = document.querySelectorAll('header .nav a');
+var section = document.querySelectorAll('section');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
@@ -9,11 +10,28 @@ menu.onclick = () =>{
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
-    nav.classList.remove('active')
+    nav.classList.remove('active');
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop -150;
+        let id= sec.getAttribute('id');
+     
+        if(top >= offset && top < offset + height){
+          
+            links.forEach(link =>{
+                
+                link.classList.remove('active');
+            })
+            document.querySelector('header .nav a[href*='+id+']').classList.add('active');
+
+        }
+    })
 }
 
 links.forEach(function(link){
-    link.onclick = () =>{
+    link.onclick = () => {
         var parent = link.parentElement;
         for(let i=0; i<parent.children.length; i++){
             parent.children[i].classList.remove('active')
@@ -21,4 +39,6 @@ links.forEach(function(link){
         link.classList.add('active');
     }
 });
+
+
 
